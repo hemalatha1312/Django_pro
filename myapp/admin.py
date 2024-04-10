32bab7799e421e4eaad9597a728080ef111b94c5
hemalatha1312 <154005603+hemalatha1312@users.noreply.github.com>
2024-04-10 10:36:45 +0530

    add

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
